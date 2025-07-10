2795ab942704d9df3f923f14bdbcf6cbb3e56752
Ostap Brehin <osbre@protonmail.com>
2025-07-11 00:56:15 +0100

    Update phpcli-and-composer.sh

--- phpcli-and-composer.sh
+++ phpcli-and-composer.sh
@@ -8,7 +8,7 @@ apt install -y lsb-release apt-transport-https ca-certificates curl
 curl -fsSL https://packages.sury.org/php/README.txt | sudo bash -
 
 # Install PHP 8.4 and required extensions for Laravel
-apt install -y unzip php8.4 php8.4-cli php8.4-mbstring php8.4-xml php8.4-mysql php8.4-curl php8.4-bcmath php8.4-zip php8.4-soap php8.4-intl php8.4-common php8.4-opcache php8.4-pgsql php8.4-sqlite3
+apt install -y unzip php8.4-cli php8.4-mbstring php8.4-xml php8.4-mysql php8.4-curl php8.4-bcmath php8.4-zip php8.4-soap php8.4-intl php8.4-common php8.4-opcache php8.4-pgsql php8.4-sqlite3
 
 # Set PHP 8.4 as default
 update-alternatives --set php /usr/bin/php8.4
