27db492c9650b38e8bb335aa08be1f281ca000d5
Ostap Brehin <osbre@protonmail.com>
2025-11-16 23:10:02 +0000

    `nginx.sh`

---
+++ nginx.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
+
+curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
+    | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
+
+gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
+
+
+echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
+http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
+    | tee /etc/apt/sources.list.d/nginx.list
+
+echo -e "Package: *\nPin: origin nginx.org\nPin: release o=nginx\nPin-Priority: 900\n" \
+    | tee /etc/apt/preferences.d/99nginx
+
+apt update
+apt install nginx -y
