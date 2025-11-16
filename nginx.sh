e6fb478f38009346ea94959e85a9b2d222557cc7
Ostap Brehin <osbre@protonmail.com>
2025-11-16 23:11:48 +0000

    Remove dry-run GPG key import command
    Removed dry-run import command for GPG key.

--- nginx.sh
+++ nginx.sh
@@ -7,9 +7,6 @@ apt install curl gnupg2 ca-certificates lsb-release debian-archive-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor \
     | tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 
-gpg --dry-run --quiet --no-keyring --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
-
-
 echo "deb [signed-by=/usr/share/keyrings/nginx-archive-keyring.gpg] \
 http://nginx.org/packages/mainline/debian `lsb_release -cs` nginx" \
     | tee /etc/apt/sources.list.d/nginx.list
