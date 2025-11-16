13316dc3d91c0f0489a448b3769ca3fac3975249
Ostap Brehin <osbre@protonmail.com>
2025-11-16 22:54:26 +0000

    Inline Sury's script

--- phpcli-and-composer.sh
+++ phpcli-and-composer.sh
@@ -5,7 +5,18 @@ apt update
 apt install -y lsb-release apt-transport-https ca-certificates curl
 
 # Add Ondřej Surý's PHP repository
-curl -fsSL https://packages.sury.org/php/README.txt | sudo bash -
+# curl -fsSL https://packages.sury.org/php/README.txt | sudo bash -
+if [ "$(whoami)" != "root" ]; then
+    SUDO=sudo
+fi
+
+${SUDO} apt-get update
+${SUDO} apt-get -y install lsb-release ca-certificates curl
+${SUDO} curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+${SUDO} dpkg -i /tmp/debsuryorg-archive-keyring.deb
+${SUDO} sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+${SUDO} apt-get update
+# end add repository
 
 # Install PHP 8.4 and required extensions for Laravel
 apt install -y unzip php8.4-cli php8.4-mbstring php8.4-xml php8.4-mysql php8.4-curl php8.4-bcmath php8.4-zip php8.4-soap php8.4-intl php8.4-common php8.4-opcache php8.4-pgsql php8.4-sqlite3 php-pear php8.4-dev g++ make
